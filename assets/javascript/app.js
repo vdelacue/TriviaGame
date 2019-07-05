3c6c8474234d29c7d861005119e6ceed4e2030cc
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-07-04 23:12:31 -0400

    added start page and a countdown gif set with a timer before questions appear on screen.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,78 +19,78 @@
 
 //create game Object with questions//
 
-var Blah = [
-    "What we've got here is failure to communicate. " {
-        "The Shawshank Redemption" : 0,
-        "Cool Hand Luke" : 1,
-        "Top Gun" : 0,
-        "" : 0
+var Blah = {
+    "What we've got here is failure to communicate. ": {
+        "The Shawshank Redemption": 0,
+        "Cool Hand Luke": 1,
+        "Top Gun": 0,
+        "Butch Cassidy and the Sundance Kid": 0
     },
 
-    "Movies don't create psychos. Movies make psychos more creative! " {
-        "" : 0,
-        "" : 0,
-        "" : 0,
-        "" : 1
+    "Movies don't create psychos. Movies make psychos more creative! ": {
+        "American Psycho": 0,
+        "Haloween H20": 0,
+        "Psycho": 0,
+        "Scream": 1
     },
 
-    "That rug really tied the room together. " {
-        "" : 0,
-        "" : 0,
-        "" : 0,
-        "" : 1
+    "That rug really tied the room together. ": {
+        "Fight Club": 0,
+        "Pulp Fiction": 0,
+        "The Big Lebowski": 1,
+        "Fargo": 0
     },
 
-    "I have been touched by your kids...and I'm pretty sure that I've touched them. " {
-        "" : 0,
-        "" : 0,
-        "" : 0,
-        "" : 1
+    "I have been touched by your kids...and I'm pretty sure that I've touched them. ": {
+        "School of Rock": 1,
+        "Kindergarten Cop": 0,
+        "Twins": 0,
+        "Bad Teacher": 0
     },
 
-    "You are a sad, strange little man and you have my pity. " {
-        "" : 0,
-        "" : 0,
-        "" : 0,
-        "" : 1
+    "You are a sad, strange little man and you have my pity. ": {
+        "The Incredibles": 0,
+        "Up": 0,
+        "Toy Story": 1,
+        "Megamind": 0
     },
 
-    " " {
-        "" : 0,
-        "" : 0,
-        "" : 0,
-        "" : 1
+    "I am serious... and don't call me Shirley. ": {
+        "Monty Python": 0,
+        "Hot Shots Part Deux": 0,
+        "Naked Gun": 0,
+        "Airplane!": 1
     },
 
-    " " {
-        "" : 0,
-        "" : 0,
-        "" : 0,
-        "" : 1
+    "I never had any friends later on like the ones I had when I was 12, Jesus, does anyone? ": {
+        "Dazed and Confused": 0,
+        "Stand By Me": 1,
+        "The Sandlot ": 0,
+        "The Mighty Ducks": 0
     },
 
-    " " {
-        "" : 0,
-        "" : 0,
-        "" : 0,
-        "" : 1
+    "Mess with the bull young man you'll get the horns. ": {
+        "Pretty in Pink": 0,
+        "Sixteen Candles": 0,
+        "Breakfast Club": 0,
+        "Say Anything": 1
     },
 
-    " " {
-        "" : 0,
-        "" : 0,
-        "" : 0,
-        "" : 1
+    "We don't need no stinking badges! ": {
+        "Casablanca": 0,
+        "Dirty Harry": 0,
+        "Treasure of the Sierra Madre": 1,
+        "The Good the Bad and the Ugly": 0
     },
 
-    " " {
-        "" : 0,
-        "" : 0,
-        "" : 0,
-        "" : 1
+    "I love the smell of napalm in the morning. ": {
+        "Platoon": 0,
+        "Saving Private Ryan": 0,
+        "Full-Metal Jacket": 0,
+        "Apocalypse Now": 1
     }
 
-]
+}
 //----------Global Variables---------//
 
 // count correct answers from player
@@ -110,3 +110,24 @@ var score = 0;
 var gameOver = false;
 
 
+//-------------funtion to start Old Movie countdown timer 5 seconds------------//
+$("#startBTN").click(function () {
+
+        $(".startScreen").hide();
+        $("#startGameScreen").append('<img src="assets/images/5SecCountdown.gif">');
+    
+    setTimeout(fiveSeconds, 1000 * 5);
+
+    function fiveSeconds() {
+        $("#startGameScreen").hide();
+    }
+});
+
+
+
+
+
+
+
+// setTimeout(tenSeconds, 1000 * 10);
+// setTimeout(timeUp, 1000 * 15);
